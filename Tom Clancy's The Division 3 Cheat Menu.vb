a297694008f662583a58d6cb27435a945ab90e62
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-05-14 16:32:20 +0200

    Add files via upload

---
+++ Tom Clancy's The Division 3 Cheat Menu.vb
@@ -0,0 +1,169 @@
+-- Cheat Engine Cheat Menu Lua Script for Tom Clancy's The Division 3
+
+-- Enable the following cheats by changing the value to 1
+
+-- God Mode (Player)
+godModePlayer = 0
+
+-- God Mode (NPC)
+godModeNPC = 0
+
+-- Infinite Ammo
+infiniteAmmo = 0
+
+-- No Reload
+noReload = 0
+
+-- One Hit Kill
+oneHitKill = 0
+
+-- Rapid Fire
+rapidFire = 0
+
+-- Super Speed
+superSpeed = 0
+
+-- Teleport to Waypoint
+teleportToWaypoint = 0
+
+-- Fly Mode
+flyMode = 0
+
+-- Infinite Medkits
+infiniteMedkits = 0
+
+-- Infinite Grenades
+infiniteGrenades = 0
+
+-- Infinite Skills
+infiniteSkills = 0
+
+-- Infinite Signature Skills
+infiniteSignatureSkills = 0
+
+-- Infinite Resources
+infiniteResources = 0
+
+-- Instant Skills Cooldown
+instantSkillsCooldown = 0
+
+-- Instant Signature Skills Cooldown
+instantSignatureSkillsCooldown = 0
+
+-- Instant Revive
+instantRevive = 0
+
+-- Unlock All Weapons
+unlockAllWeapons = 0
+
+-- Unlock All Skills
+unlockAllSkills = 0
+
+-- Unlock All Perks
+unlockAllPerks = 0
+
+-- Unlock All Mods
+unlockAllMods = 0
+
+-- Unlock All Clothing
+unlockAllClothing = 0
+
+-- Unlock All Vanity Items
+unlockAllVanityItems = 0
+
+-- Unlock All Commendations
+unlockAllCommendations = 0
+
+-- Unlock All Achievements
+unlockAllAchievements = 0
+
+-- Set Player Level
+setPlayerLevel = 0
+playerLevel = 30
+
+-- Set Player Gear Score
+setPlayerGearScore = 0
+playerGearScore = 500
+
+-- Set Player Credits
+setPlayerCredits = 0
+playerCredits = 999999
+
+-- Set Player Phoenix Credits
+setPlayerPhoenixCredits = 0
+playerPhoenixCredits = 9999
+
+-- Set Player Dark Zone Level
+setPlayerDarkZoneLevel = 0
+playerDarkZoneLevel = 99
+
+-- Set Player Dark Zone Funds
+setPlayerDarkZoneFunds = 0
+playerDarkZoneFunds = 999999
+
+-- Set Player Dark Zone Keys
+setPlayerDarkZoneKeys = 0
+playerDarkZoneKeys = 99
+
+-- Set Player Dark Zone Experience
+setPlayerDarkZoneExperience = 0
+playerDarkZoneExperience = 999999
+
+-- Set Player Dark Zone Rogue Status
+setPlayerDarkZoneRogueStatus = 0
+playerDarkZoneRogueStatus = 0
+
+-- Set Player Dark Zone Rogue Timer
+setPlayerDarkZoneRogueTimer = 0
+playerDarkZoneRogueTimer = 0
+
+-- Set Player Dark Zone Rogue Kills
+setPlayerDarkZoneRogueKills = 0
+playerDarkZoneRogueKills = 0
+
+-- Set Player Dark Zone Rogue Deaths
+setPlayerDarkZoneRogueDeaths = 0
+playerDarkZoneRogueDeaths = 0
+
+-- Set Player Dark Zone Rogue Manhunt Status
+setPlayerDarkZoneRogueManhuntStatus = 0
+playerDarkZoneRogueManhuntStatus = 0
+
+-- Set Player Dark Zone Rogue Manhunt Timer
+setPlayerDarkZoneRogueManhuntTimer = 0
+playerDarkZoneRogueManhuntTimer = 0
+
+-- Set Player Dark Zone Rogue Manhunt Kills
+setPlayerDarkZoneRogueManhuntKills = 0
+playerDarkZoneRogueManhuntKills = 0
+
+-- Set Player Dark Zone Rogue Manhunt Deaths
+setPlayerDarkZoneRogueManhuntDeaths = 0
+playerDarkZoneRogueManhuntDeaths = 0
+
+-- Set Player Dark Zone Rogue Manhunt Experience
+setPlayerDarkZoneRogueManhuntExperience = 0
+playerDarkZoneRogueManhuntExperience = 0
+
+-- Set Player Dark Zone Rogue Manhunt Bounty
+setPlayerDarkZoneRogueManhuntBounty = 0
+playerDarkZoneRogueManhuntBounty = 0
+
+-- Set Player Dark Zone Rogue Manhunt Timer Multiplier
+setPlayerDarkZoneRogueManhuntTimerMultiplier = 0
+playerDarkZoneRogueManhuntTimerMultiplier = 1
+
+-- Set Player Dark Zone Rogue Manhunt Kills Multiplier
+setPlayerDarkZoneRogueManhuntKillsMultiplier = 0
+playerDarkZoneRogueManhuntKillsMultiplier = 1
+
+-- Set Player Dark Zone Rogue Manhunt Deaths Multiplier
+setPlayerDarkZoneRogueManhuntDeathsMultiplier = 0
+playerDarkZoneRogueManhuntDeathsMultiplier = 1
+
+-- Set Player Dark Zone Rogue Manhunt Experience Multiplier
+setPlayerDarkZoneRogueManhuntExperienceMultiplier = 0
+playerDarkZoneRogueManhuntExperienceMultiplier = 1
+
+-- Set Player DarkZone Rogue Manhunt Bounty Multiplier
+setPlayerDarkZoneRogueManhuntBountyMultiplier =
